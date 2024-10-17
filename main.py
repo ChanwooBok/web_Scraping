192314d410ee4fd05e74d08f0d4afec55e52a698
Chanwoo <titoddd@naver.com>
2024-10-17 16:16:11 +0900

    i di  it

--- main.py
+++ main.py
@@ -1,47 +1,12 @@
-import cloudscraper
-from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
 
+try:
+    p = sync_playwright().start()
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://google.com")
+    page.screenshot(path="screenshot.png")
 
-
-all_jobs=[]
-url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
-
-def scrape(url):
-  print(f"scraping {url}")
-  scraper = cloudscraper.create_scraper()
-
-  response = scraper.get(url)
-
-  potato = BeautifulSoup(response.content, "html.parser")
-  jobs = potato.find("section", class_="jobs").find_all("li")[1:-1]
-
-  for job in jobs:     
-    title = job.find("span",class_="title").text
-    company,position,region = job.find_all("span",class_="company")
-    url = job.find("div",class_="tooltip--flag-logo").next_sibling["href"]
-    job_data = {
-      "title":title,
-      "company":company.text,
-      "position":position.text,
-      "region":region.text,
-      "url":f"https://weworkremotely.com{url}"
-    }
-    all_jobs.append(job_data)
-
-def get_pages(url):
-  
-  scraper = cloudscraper.create_scraper()
-  response = scraper.get("https://weworkremotely.com/remote-full-time-jobs?page=1")
-  potato = BeautifulSoup(response.content, "html.parser")
-  return len(potato.find("div",class_="pagination").find_all("span",class_="page"))
-
-total_pages = get_pages("https://weworkremotely.com/remote-full-time-jobs?page=1")
-
-for x in range(total_pages):
-    url = f"https://weworkremotely.com/remote-full-time-jobs?page={x+1}"
-    scrape(url)
-
-print(len(all_jobs))
-
-
+except Exception as e:
+    print(f"An error occurred: {e}")
 
