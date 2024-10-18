1e3b048e6924f24ec801459c6872601f52082179
Chanwoo <titoddd@naver.com>
2024-10-19 00:35:07 +0900

    search

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from playwright.sync_api import sync_playwright
+import time
 
 try:
     p = sync_playwright().start()
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
-    page.goto("https://google.com")
-    page.screenshot(path="screenshot.png")
+    page.goto("https://www.wanted.co.kr/search")
+    page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
+    page.keyboard.down("Enter")
+    time.sleep(5)
+    
+
 
 except Exception as e:
     print(f"An error occurred: {e}")
