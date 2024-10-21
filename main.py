d5916f4c919f88e4f0ce0d8a45ce2aa24f503562
Chanwoo <titoddd@naver.com>
2024-10-21 22:55:02 +0900

    getting html

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from playwright.sync_api import sync_playwright
 import time
+from bs4 import BeautifulSoup
+
 
 try:
     p = sync_playwright().start()
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
     page.goto("https://www.wanted.co.kr/search")
-    page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
+    page.get_by_placeholder("검색어를 입력해 주세요.").fill("devops")
     page.keyboard.down("Enter")
-    time.sleep(5)
     
+    page.click("a#search_tab_position")
 
+    for x in range(4):
+        page.keyboard.down("End")
+        time.sleep(3)
+    content = page.content()
+    
+    soup = BeautifulSoup(content, "html.parser")
 
 except Exception as e:
     print(f"An error occurred: {e}")
