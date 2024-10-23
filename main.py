dc25632cf8d7c2d4038a31d02c010b52bf259cc9
Chanwoo <titoddd@naver.com>
2024-10-23 15:00:33 +0900

    open and save csv file

--- main.py
+++ main.py
@@ -1,24 +1,51 @@
 from playwright.sync_api import sync_playwright
 import time
 from bs4 import BeautifulSoup
-
+import csv
 
 try:
+
     p = sync_playwright().start()
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
-    page.goto("https://www.wanted.co.kr/search")
-    page.get_by_placeholder("검색어를 입력해 주세요.").fill("devops")
-    page.keyboard.down("Enter")
+    page.goto("https://www.wanted.co.kr/search?query=devops&tab=position")
+    # page.goto("https://www.wanted.co.kr/search")
     
-    page.click("a#search_tab_position")
+    # page.get_by_placeholder("검색어를 입력해 주세요.").fill("devops")
+    # page.keyboard.down("Enter")
+    
+    # page.click("a#search_tab_position")
 
-    for x in range(4):
+    for x in range(3):
         page.keyboard.down("End")
         time.sleep(3)
+
     content = page.content()
     
     soup = BeautifulSoup(content, "html.parser")
+
+    jobs = soup.find_all("div", class_="JobCard_container__REty8")
+    jobs_list = []
+    for job in jobs :
+        link = f"https://www.wanted.co.kr{job.find('a')['href']}"
+        title = job.find("strong",class_="JobCard_title__HBpZf").text
+        company_name = job.find("span",class_="JobCard_companyName__N1YrF").text
+
+        job = {
+            "title" : title,
+            "company_name" : company_name,
+            "link" : link
+        }
+        jobs_list.append(job)
+
+    with open("jobs.csv", "w", newline="", encoding="utf-8") as file:  # Use 'with' for better file handling
+        writer = csv.writer(file)
+        writer.writerow(["Title", "Company"])  # Header
+
+        for job in jobs_list:
+            print("test")
+            print(job.values())
+            writer.writerow(job.values())
 
 except Exception as e:
     print(f"An error occurred: {e}")
