d5c552b5687346a48f7735bd8ca8fe836d28f67f
Chanwoo <titoddd@naver.com>
2024-10-25 23:19:32 +0900

    making server

--- main.py
+++ main.py
@@ -1,52 +1,13 @@
-from playwright.sync_api import sync_playwright
-import time
-from bs4 import BeautifulSoup
-from file import save_to_file
+from flask import Flask, render_template
 
-try:
-    keyword = input("what do you want to search for?")
+app = Flask("JobScrapper")
 
-    p = sync_playwright().start()
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto(f"https://www.wanted.co.kr/search?query={keyword}&tab=position")
-    # page.goto("https://www.wanted.co.kr/search")
-    
-    # page.get_by_placeholder("검색어를 입력해 주세요.").fill("devops")
-    # page.keyboard.down("Enter")
-    
-    # page.click("a#search_tab_position")
+@app.route("/")
+def home():
+    return "hello world! <a href='/hello'>go to there </a>"
 
-    for x in range(3):
-        page.keyboard.down("End")
-        time.sleep(2)
+@app.route("/hello")
+def hello():
+    return render_template("home.html",name="Chanwoo")
 
-    content = page.content()
-    
-    soup = BeautifulSoup(content, "html.parser")
-
-    jobs = soup.find_all("div", class_="JobCard_container__REty8")
-    jobs_list = []
-    for job in jobs :
-        link = f"https://www.wanted.co.kr{job.find('a')['href']}"
-        title = job.find("strong",class_="JobCard_title__HBpZf").text
-        company_name = job.find("span",class_="JobCard_companyName__N1YrF").text
-
-        job = {
-            "title" : title,
-            "company_name" : company_name,
-            "link" : link
-        }
-        jobs_list.append(job)
-
-    # with open("jobs.csv", "w", newline="", encoding="utf-8") as file:  # Use 'with' for better file handling
-    #     writer = csv.writer(file)
-    #     writer.writerow(["Title", "Company"])  # Header
-
-    #     for job in jobs_list:
-    #         writer.writerow(job.values())
-
-    save_to_file(keyword, jobs_list)
-
-except Exception as e:
-    print(f"An error occurred: {e}")
+app.run(debug=True)
--- practice2.py
+++ practice2.py
@@ -1,47 +1,47 @@
-from playwright.sync_api import sync_playwright
-import time
+import cloudscraper
 from bs4 import BeautifulSoup
-import csv
-
-
-url = "https://www.naver.com/"
-
-try : 
-
-        p = sync_playwright().start()
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
-        page.goto(url)
-        content = page.content()
-        soup = BeautifulSoup(content, "html.parser")
-        time.sleep(3)
-
-        
-        items = soup.find_all("li", class_="shortcut_item")
-        print(items)
-
-        items_db = []
-        
-        for item in items : 
-            print("start")
-            title = item.find("span",class_="service_name")
-            link = item.find("a")["href"]
-            print(title, link)
-            item = {
-                "title" : title,
-                "link" : link
-            }
-
-            items_db.append(item)
-
-        print(len(items_db))
-   
 
 
 
+all_jobs=[]
+url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
 
-except Exception as e :
+def scrape(url):
+  print(f"scraping {url}")
+  scraper = cloudscraper.create_scraper()
 
-    print(f"error occured {e}")
+  response = scraper.get(url)
 
-    
+  potato = BeautifulSoup(response.content, "html.parser")
+  jobs = potato.find("section", class_="jobs").find_all("li")[1:-1]
+
+  for job in jobs:     
+    title = job.find("span",class_="title").text
+    company,position,region = job.find_all("span",class_="company")
+    url = job.find("div",class_="tooltip--flag-logo").next_sibling["href"]
+    job_data = {
+      "title":title,
+      "company":company.text,
+      "position":position.text,
+      "region":region.text,
+      "url":f"https://weworkremotely.com{url}"
+    }
+    all_jobs.append(job_data)
+
+def get_pages(url):
+  
+  scraper = cloudscraper.create_scraper()
+  response = scraper.get("https://weworkremotely.com/remote-full-time-jobs?page=1")
+  potato = BeautifulSoup(response.content, "html.parser")
+  return len(potato.find("div",class_="pagination").find_all("span",class_="page"))
+
+total_pages = get_pages("https://weworkremotely.com/remote-full-time-jobs?page=1")
+
+for x in range(total_pages):
+    url = f"https://weworkremotely.com/remote-full-time-jobs?page={x+1}"
+    scrape(url)
+
+print(len(all_jobs))
+
+
+
