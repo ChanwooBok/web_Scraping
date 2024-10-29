72add5ae833686ba93a2c9b1cd97224b54a475b8
Chanwoo <titoddd@naver.com>
2024-10-29 22:44:51 +0900

    saving in db

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from practice1 import scraping
 
 app = Flask("JobScrapper")
 
+
+db = {}
+
+
 @app.route("/")
 def home():
     return "hello world! <a href='/hello'>go to there </a>"
@@ -16,7 +20,13 @@ def hello():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
-    jobs = scraping(keyword)
+    if keyword in db:
+        jobs = db[keyword]
+        print("this keyword is already in the db")
+    else:
+        jobs = scraping(keyword)
+        db[keyword] = jobs
+        print("db is updated and db is ",db)
     return render_template("search.html", keyword = keyword ,jobs = jobs)
 
 app.run(debug=True)
