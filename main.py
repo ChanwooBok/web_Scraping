e07bf0b7f5217e52bfc610cc273e0845dbdf1dc5
Chanwoo <titoddd@naver.com>
2024-10-30 23:39:40 +0900

    downloading csv file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 from practice1 import scraping
-
+from file import save_to_file
 
 
 app = Flask("JobScrapper")
@@ -26,7 +26,17 @@ def search():
     else:
         jobs = scraping(keyword)
         db[keyword] = jobs
-        print("db is updated and db is ",db)
     return render_template("search.html", keyword = keyword ,jobs = jobs)
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword is None:
+        return redirect("/")
+    if keyword not in db:
+        print("keyword is not in db")
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
 
 app.run(debug=True)
